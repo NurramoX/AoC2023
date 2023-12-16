d61f0dffa51d5f4be8ae80f630df17d2dfc5f95d
Ramzan Magomadow <r_magomadow@protonmail.com>
2023-12-16 13:21:58 +0100

    feat: ChatGPT improvements

--- Day_1/Bun/index.ts
+++ Day_1/Bun/index.ts
@@ -1,67 +1,40 @@
 import * as fs from "fs";
-import _ from "lodash";
 
-// const payload = fs.readFileSync("../payload.txt", "utf-8");
-
-const payload = `two1nine 
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen
-`
+const payload = fs.readFileSync("../payload.txt", "utf-8");
 
 const mappings: string[] = [
-    'one',
-    'two',
-    'three',
-    'four',
-    'five',
-    'six',
-    'seven',
-    'eight',
-    'nine'
+    'one', 'two', 'three', 'four', 'five',
+    'six', 'seven', 'eight', 'nine'
 ];
 
-const revMappings: typeof mappings = _(mappings).map(str => _(str).split('').reverse().join('')).value();
+// Function to reverse a string
+const reverseString = (str: string) => str.split('').reverse().join('');
 
-const enumerateMappings = mappings.reduce((obj, item, index) => {
-    obj[item] = ""+(index+1);
+// Create reverse mappings from the original mappings
+const revMappings: typeof mappings = mappings.map(reverseString);
+
+// Function to create enumeration mappings from an array of strings
+const createEnumerationMappings = (arr: string[]) => arr.reduce((obj, item, index) => {
+    obj[item] = (index + 1).toString();
     return obj;
-}, {} as { [key: string]: string});
+}, {} as { [key: string]: string });
 
-const enumerateRevMappings = revMappings.reduce((obj, item, index) => {
-    obj[item] = ""+(index+1);
-    return obj;
-}, {} as { [key: string]: string});
+const enumerateMappings = createEnumerationMappings(mappings);
+const enumerateRevMappings = createEnumerationMappings(revMappings);
 
+// Compile regular expressions outside of mapping functions
 const regex = new RegExp(`(${mappings.join('|')}|\\d)`);
 const revRegex = new RegExp(`(${revMappings.join('|')}|\\d)`);
-const processed = payload
-    .split('\n')
-    .map(str => _.find(str.match(regex)))
-    .map(str => {
-        if (str === undefined) return "0";
-        else if (enumerateMappings.hasOwnProperty(str)) {
-           return enumerateMappings[str];
-        } else {
-            return str;
-        }
-    });
 
-const revProcessed = payload.split('\n')
-    .map(str => str.split('').reverse().join(''))
-    .map(str => _.find(str.match(revRegex)))
-    .map(str => {
-        if (str === undefined) return "0";
-        else if (enumerateRevMappings.hasOwnProperty(str)) {
-            return enumerateRevMappings[str];
-        } else {
-            return str;
-        }
-    })
-const total = _
-    .zipWith(processed, revProcessed, (a, b) => _.toInteger(a+b))
-    .reduce((a, b) => a+b);
+// Process the payload and calculate total
+const total = payload.split('\n').reduce((sum, line) => {
+    const normalMatch = line.match(regex)?.[0];
+    const reverseMatch = reverseString(line).match(revRegex)?.[0];
+
+    const normalValue = normalMatch ? enumerateMappings[normalMatch] || normalMatch : '0';
+    const reverseValue = reverseMatch ? enumerateRevMappings[reverseMatch] || reverseMatch : '0';
+
+    return sum + parseInt(normalValue + reverseValue, 10);
+}, 0);
+
 console.log(total);
